e0603ea18bb93a107c0c6211b3ba759fb91252b4
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-03-30 14:51:33 -0400

    Querying database

--- backend/upload.py
+++ backend/upload.py
@@ -1,3 +1,5 @@
+# https://python.langchain.com/docs/integrations/vectorstores/redis
+# https://cloud.google.com/blog/products/databases/memorystore-for-redis-vector-search-and-langchain-integration
 import os
 import csv
 import dotenv
@@ -10,7 +12,7 @@ dotenv.load_dotenv()
 REDIS_URL = os.getenv("redis_url")
 
 
-def initialize_database():
+def initialize_database() -> Redis:
     embedding = VertexAIEmbeddings(model_name="textembedding-gecko@001")
     index_name = "jobs"
     if os.path.isfile("vectorstore.pkl"):
@@ -24,7 +26,8 @@ def initialize_database():
         metadata = list()
         texts = list()
         for jobs in data:
-            metadata.append({"job_title": jobs[0], "location": jobs[1], "salary": jobs[2], "job_type": jobs[3], "description": jobs[4]})
+            metadata.append({"job_title": jobs[0], "location": jobs[1], "salary": jobs[2], "job_type": jobs[3],
+                             "description": jobs[4]})
             texts.append(jobs[4])
         vectorstore = Redis.from_texts(texts, embedding, metadatas=metadata, redis_url=REDIS_URL, index_name=index_name)
         with open("vectorstore.pkl", "wb") as f:
@@ -33,10 +36,19 @@ def initialize_database():
     return vectorstore
 
 
-def open_csv(file_name: str):
+def open_csv(file_name: str) -> list:
     with open(file_name, mode='r') as file:
         data = list(csv.reader(file))
         return data
 
 
-initialize_database()
+def query_database(vectorstore: Redis, query: str, num_results: int = 5) -> list:
+    results = vectorstore.similarity_search_with_relevance_scores(query, k=num_results)
+    return results
+
+
+vectorstore = initialize_database()
+results = query_database(vectorstore, "developer software engineer", 3)
+print(results)
+for result in results:
+    print(f"Content: {result[0].page_content} --- Similiarity: {result[1]} --- Metadata: {result[0].metadata}")
